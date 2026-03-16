0107d9531a43b5c2506d77588dfde3c98c7f0a04
Emmanuel <58248939+w3bscott@users.noreply.github.com>
2026-03-16 11:07:08 +0100

    aded key test2

--- scriptMemoryFlip.js
+++ scriptMemoryFlip.js
@@ -23,7 +23,7 @@ clap = document.querySelector('#clap');
   
 reset();
 
-const OPENAI_API_KEY = 'co_67vd7v4376g4378909nejkueibew8742878vweh';
+const ANTHROPIC_API_KEY = 'sk-ant-api03-7f2b9c4a1d6e8f0b3c5a9d2e7f4b6c8a1d3e5f7c9b2a4d6e8f1c3a5b7d9e2';
 
 
 // for(var i = 0; i < cardNum.length; i++){
